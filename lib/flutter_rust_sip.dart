07aba32b89d511a6cc9cd56b82f00bd196af7b2e
Mortie <38497452+omortie@users.noreply.github.com>
2025-09-04 16:10:21 +0200

    working plugin

--- lib/flutter_rust_sip.dart
+++ lib/flutter_rust_sip.dart
@@ -1,6 +1,7 @@
-library;
+library flutter_rust_sip;
 
 export 'rust/api/simple.dart';
 export 'rust/frb_generated.dart' show RustLib;
-export 'rust/core/dart_types.dart';
-export 'rust/core/types.dart';
+export 'rust/core/dart_types.dart' show CallInfo, CallState;
+export 'rust/core/types.dart'
+    show TelephonyError, TransportMode, OnIncommingCall;
--- lib/rust/frb_generated.dart
+++ lib/rust/frb_generated.dart
@@ -72,7 +72,7 @@ class RustLib extends BaseEntrypoint<RustLibApi, RustLibApiImpl, RustLibWire> {
 
   static const kDefaultExternalLibraryLoaderConfig =
       ExternalLibraryLoaderConfig(
-        stem: 'rust_sip',
+        stem: 'flutter_rust_sip',
         ioDirectory: 'rust/target/release/',
         webPrefix: 'pkg/',
       );
