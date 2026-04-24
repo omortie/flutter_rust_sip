21a50d47c89dc0af2b017115190eab1228748563
Mortie <38497452+omortie@users.noreply.github.com>
2026-04-24 21:36:28 +0300

    removed experimental hook script

--- hook/build.dart
+++ hook/build.dart
@@ -1,13 +1,64 @@
+import 'dart:io' show Platform, File;
+
 import 'package:hooks/hooks.dart';
 import 'package:native_toolchain_rust/native_toolchain_rust.dart';
-
-import 'env_utilizer.dart' show Env;
 
 const _pkgConfigSysrootx8664EnvVar =
     'PKG_CONFIG_SYSROOT_DIR_x86_64_linux_android';
 const _pkgConfigSysrootAarch64EnvVar =
     'PKG_CONFIG_SYSROOT_DIR_aarch64_linux_android';
 const _androidNDKHomeEnvVar = 'ANDROID_NDK_HOME';
+
+class Env {
+  Map<String, String> _env = {};
+
+  static final Env instance = Env._();
+
+  Env._() {
+    // read cwd .env
+    _env = Platform.environment.map((key, value) => MapEntry(key, value));
+    // find $HOME/frtp_build.env file
+    final home =
+        Platform.environment['HOME'] ?? Platform.environment['USERPROFILE'];
+    final file = File('$home/cross_build.env');
+    // coverage:ignore-start
+    if (file.existsSync()) {
+      final lines = file.readAsLinesSync();
+      for (final line in lines) {
+        int index = line.indexOf("=");
+        if (index != -1) {
+          _env[line.substring(0, index).trim()] = line
+              .substring(index + 1)
+              .trim();
+        }
+      }
+    }
+    // coverage:ignore-end
+  }
+  void set(String key, String value) {
+    _env[key] = value;
+  }
+
+  void setDefault<T>(String key, T value) {
+    _env.putIfAbsent(key, () => value.toString());
+  }
+
+  void setBool(String key, bool value) {
+    set(key, value.toString());
+  }
+
+  String getString(String key, {String defaultValue = ''}) {
+    return _env[key] ?? defaultValue;
+  }
+
+  bool getBool(String key, {bool defaultValue = false}) {
+    final found = _env[key];
+    if (found == null) {
+      return defaultValue;
+    }
+    return found.toLowerCase() == 'true';
+  }
+}
 
 void main(List<String> args) async {
     // we need to read an standard env file in a known-well path `$HOME/cross_build.env` to get the env vars for building, 
